fbecb2ac997c03a957ded736765370cae3c90176
Peter Tseng <pht24@cornell.edu>
2022-12-11 10:23:18 +0000

    day 11: Itemwise (slower)

--- bin/11_monkey_in_the_middle.hs
+++ bin/11_monkey_in_the_middle.hs
@@ -5,10 +5,10 @@ import AdventOfCode.Split (splitOn)
 
 import Prelude hiding (round)
 
-import Data.Array ((!), Array, bounds, listArray)
+import Data.Array ((!), Array, listArray)
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as IntMap
-import Data.List (dropWhileEnd, foldl', sortBy)
+import Data.List (dropWhileEnd, sortBy, unfoldr)
 
 data Monkey = Monkey {
   pos :: Int
@@ -23,38 +23,22 @@ business :: [Monkey] -> (Int -> Int) -> Int -> Int
 business monkeys worry nrounds = product (take 2 actives)
   where monkeysArr = listArray (0, length monkeys - 1) monkeys
         items = concatMap (\monkey -> map (, pos monkey) (startingItems monkey)) monkeys
-        inspects = foldl' (IntMap.unionWith (+)) IntMap.empty (map (uncurry (itemInspects worry monkeysArr nrounds)) items)
+        inspects = freqMap (concatMap (uncurry (poses worry monkeysArr nrounds)) items)
         actives = sortBy (flip compare) (IntMap.elems inspects)
 
-itemInspects :: (Int -> Int) -> Array Int Monkey -> Int -> Int -> Int -> IntMap Int
-itemInspects worry monkeys = insp IntMap.empty IntMap.empty Nothing
-  where
-    insp :: IntMap Int -> IntMap Int -> Maybe (Int, Int, IntMap Int) -> Int -> Int -> Int -> IntMap Int
-    -- finished
-    insp inspects _ _ 0 _ _ = inspects
-    -- cycle complete, skip forward
-    insp inspects history (Just (cycleLen, 0, inspectsInCycle)) nrounds item itemPos = insp inspects' history (Just (cycleLen, -1, inspectsInCycle)) nrounds' item itemPos
-      where completeCycle = nrounds `quot` cycleLen
-            nrounds' = nrounds - completeCycle * cycleLen
-            inspects' = IntMap.unionWith (+) inspects (IntMap.map (* completeCycle) inspectsInCycle)
-    -- all other cases
-    insp inspects history cycled nrounds item itemPos = insp inspects' history' cycled' nrounds' item' dest
-      where nmonkeys = snd (bounds monkeys) + 1
-            monkey = monkeys ! itemPos
-            item' = worry (op monkey item)
-            dest = (if item' `rem` divisor monkey == 0 then ifTrue else ifFalse) monkey
-            inspects' = IntMap.insertWith (+) itemPos 1 inspects
-            cacheKey = item' * nmonkeys + dest
-            (cycled', history', nrounds') = case cycled of
-              -- in the middle of recording a cycle
-              Just (cycleLen, roundsInCycle, inspectsInCycle) -> (Just (cycleLen, roundsInCycle - dround, inspectsInCycle'), history, nrounds - dround)
-                where inspectsInCycle' = IntMap.insertWith (+) itemPos 1 inspectsInCycle
-                      dround = if dest < itemPos then 1 else 0
-              -- still in the same round
-              Nothing | dest >= itemPos -> (cycled, history, nrounds)
-              -- new round, check if we found a cycle
-              Nothing -> (fmap (\prev -> let len = prev - nrounds in (len, len, IntMap.empty)) (IntMap.lookup cacheKey history), IntMap.insert cacheKey nrounds history, nrounds - 1)
+poses :: (Int -> Int) -> Array Int Monkey -> Int -> Int -> Int -> [Int]
+poses worry monkeys nrounds item itemPos = unfoldr (turn worry monkeys) (nrounds, item, itemPos)
 
+turn :: (Int -> Int) -> Array Int Monkey -> (Int, Int, Int) -> Maybe (Int, (Int, Int, Int))
+turn _ _ (0, _, _) = Nothing
+turn worry monkeys (nrounds, item, itemPos) = Just (itemPos, (nrounds', item', dest))
+  where monkey = monkeys ! itemPos
+        item' = worry (op monkey item)
+        dest = (if item' `rem` divisor monkey == 0 then ifTrue else ifFalse) monkey
+        nrounds' = nrounds - if dest < itemPos then 1 else 0
+
+freqMap :: [Int] -> IntMap Int
+freqMap = IntMap.fromListWith (+) . map (, 1)
 
 readMonkey :: [String] -> Monkey
 readMonkey [poss, start, ops, divi, ift, iff] = Monkey {
